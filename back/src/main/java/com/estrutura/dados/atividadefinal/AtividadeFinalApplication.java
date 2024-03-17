eb1c3bf8b58a3df237d29ae31e13b8dc9427d2ff
Igor Souza <88636711+igorsouzaccruz@users.noreply.github.com>
2024-03-17 16:14:29 -0300

    first commit

---
+++ back/src/main/java/com/estrutura/dados/atividadefinal/AtividadeFinalApplication.java
@@ -0,0 +1,13 @@
+package com.estrutura.dados.atividadefinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtividadeFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtividadeFinalApplication.class, args);
+	}
+
+}
---
+++ back/src/test/java/com/estrutura/dados/atividadefinal/AtividadeFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estrutura.dados.atividadefinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtividadeFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
